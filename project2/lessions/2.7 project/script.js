3264d526af4c5af102bea9763df0569feb89b287
Vasyl <mr.algumin@gmail.com>
2021-07-25 22:22:44 -0500

    whole folder

---
+++ project2/lessions/2.7 project/script.js
@@ -0,0 +1,23 @@
+'use strict'
+
+let money = prompt("Ваш бютжет на месяц?",'');
+let time = prompt("Введите дату в формате YYYY-MM-DD",'YYYY-MM-DD');
+let a1 = prompt("Введите обязательную статью расходов в этом месяце",'');
+let a2 = prompt("Во сколько обойдется?",'');
+let a3 = prompt("Введите обязательную статью расходов в этом месяце",'');
+let a4 = prompt("Во сколько обойдется?",'');
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert("бюджет на 1 день" + money/30);
+
